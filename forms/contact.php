041bdca5d51318f15afa6c8560570feca0548936
kartikup99 <91777911+kartikup99@users.noreply.github.com>
2021-10-13 17:32:21 +0530

    sd

--- forms/contact.php
+++ forms/contact.php
@@ -1,9 +1,15 @@
 <?php
-    $toEmail = "lunargrids@gmial.com";
-    $mailHeaders = "From: " . $_POST["name"] . "<". $_POST["email"] .">\r\n";
-    if(mail($toEmail, $_POST["subject"], $_POST["message"], $mailHeaders)) {
-        print "<p class='success'>Mail Sent.</p>";
-    } else {
-        print "<p class='Error'>Problem in Sending Mail.</p>";
+if(isset($_POST['submit'])){
+    $to = "lunargrids@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
     }
 ?>
